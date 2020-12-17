7e1f793164de317d8808c1b73889c0bf32720bae
serlapteff <ser.lapteff@yandex.ru>
2020-12-17 18:12:10 +0300

    Solution to task 01-basics - CounterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -2,3 +2,17 @@ import Vue from './vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+
+const app = new Vue({
+  el: '#btn',
+  data() {
+    return {
+      value: 0
+    }
+  },
+  methods: {
+    clickHandler(e) {
+      this.value = this.value + 1;
+    },
+  },
+});
