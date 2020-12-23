91da3c303cf73ec7f3c3006713e3ad4a0a5a5b38
serlapteff <ser.lapteff@yandex.ru>
2020-12-23 16:53:54 +0300

    Solution to task 02-basics - CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,30 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="clickHandler">{{ counter }}</button>',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+      // validator: (value) => value.typeof === 'number',
+    },
+  },
+  data: function () {
+    return {
+      counter: this.count
+    }
+  },
 
   // Компонент должен иметь модель
-
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    clickHandler(e) {
+      this.$emit('increment', this.counter = this.counter + 1)
+    },
+  },
 };
