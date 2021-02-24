5c4b144ac8553ed20d47bd025a0698f3e93277cb
s.laptev <s.laptev@agatgroup.com>
2021-02-24 17:33:58 +0300

    Solution to task 07-forms - 01-CounterButton_last_final

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,11 +1,41 @@
 <template>
-  <button></button>
+  <button type="button" @click="handaleClick">{{ localCount }}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      localCount: this.count
+    }
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+  watch: {
+    count: {
+      handler(newValue) {
+        return this.localCount = newValue
+      },
+    },
+  },
+  methods: {
+    handaleClick() {
+      this.$emit('increment', this.localCount += 1)
+    },
+  }
+};
 </script>
 
 <style>
-
+button {
+  width: 100px;
+}
 </style>
