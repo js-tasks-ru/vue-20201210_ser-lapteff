fc43e0c4f5a97998abb89ad5475c66c9801993d3
s.laptev <s.laptev@agatgroup.com>
2021-03-05 20:55:51 +0300

    Solution to task 08-interactions-and-logic-reuse - 02-AppPagination

--- 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <!-- Решение -->
+    <template v-for="item in slicedItems">
+      <slot :item="item"></slot>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppPagination',
+  props: {
+    page: Number,
+    perPage: Number,
+    items: Array
+  },
+  computed: {
+    slicedItems() {
+      const startPos = (this.page - 1) * this.perPage;
+      const endPos = startPos + this.perPage;
+      return this.items.slice(startPos, endPos)
+    }
+  }
 };
 </script>
 
